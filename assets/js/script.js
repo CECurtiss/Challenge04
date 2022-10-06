775f2377d7ead1e67b5971fde6d5998905b94928
CECurtiss <curtissclinton@gmail.com>
2022-10-05 22:59:13 -0500

    added variables in js and event listenters for buttons. named some functions

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var timerEl = document.querySelector("#timer");
+var titleBoxEl = document.querySelector(".titlebox");
+var startQuizBtn = document.querySelector("#startquizbtn");
+var highScores = document.querySelector("#highscores");
+
+
+function showHighScores(){
+    
+    
+    // hide all body text and load local storage
+}
+highScores.addEventListener("click", showHighScores());
+
+
+function beginQuiz() {
+startQuizBtn.addEventListener("click",function() {
+// function to hide original text and show question 1
+} )
+}
+// create display hide for main screen
+
+// create timer function
+
+// write questions in html
+
+// create buttons for answers and link them to js using if statement
+
+// 
